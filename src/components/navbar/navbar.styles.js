fa58ce3f1e2eca0b42c97d42860579854dac618a
Bartoszkie <bartosz.slysz23@gmail.com>
2020-01-29 17:36:17 +0100

    check 2

--- src/components/navbar/navbar.styles.js
+++ src/components/navbar/navbar.styles.js
@@ -39,7 +39,7 @@ export const BackgroundNav = styled.nav`
   top: 0;
   left: 0;
   opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
-  transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  transition: all 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55);
 
   text-align: center;
 
